08712c94c62e3594f6a91ebabb588c398739b439
Steven Zhou <stevenz9@uci.edu>
2023-04-13 23:26:10 -0700

    created form component and in progress to post form info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,24 @@
 import "./App.css";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
-	return (
-		<div className="App">
-			{/* TODO: include an icon for the quote book */}
-			<Header/>
-			
-			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
-				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
-				<button type="submit">Submit</button>
-			</form>
-
-			<h2>Previous Quotes</h2>
-			{/* TODO: Display the actual quotes from the database */}
-			<div className="messages">
-				<p>Peter Anteater</p>
-				<p>Zot Zot Zot!</p>
-				<p>Every day</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* TODO: include an icon for the quote book */}
+      <Header />
+      <h2>Submit a quote</h2>
+      {/* TODO: implement custom form submission logic to not refresh the page */}
+      <Form />
+      <h2>Previous Quotes</h2>
+      {/* TODO: Display the actual quotes from the database */}
+      <div className="messages">
+        <p>Peter Anteater</p>
+        <p>Zot Zot Zot!</p>
+        <p>Every day</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function Form() {
+  const [formInfo, setFormInfo] = React.useState({
+    name: "",
+    message: "",
+    time: "",
+    maxAge: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormInfo((prevFormInfo) => {
+      return {
+        ...prevFormInfo,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formInfo);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="input-name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="input-name"
+        value={formInfo.name}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor="input-message">Quote</label>
+      <input
+        type="text"
+        name="message"
+        id="input-message"
+        value={formInfo.message}
+        onChange={handleChange}
+        required
+      />
+      <label htmlFor="max-age">Quote Age</label>
+      <select name="maxAge" id="max-age" onChange={handleChange}>
+        <option value="">---Choose an option---</option>
+        <option value="year">Up to a year ago</option>
+        <option value="month">Up to a month ago</option>
+        <option value="week">Up to a week ago</option>
+        <option value="all">All the quotes</option>
+      </select>
+      <button>Submit</button>
+    </form>
+  );
+}
